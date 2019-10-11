4942e6aa592a547e4a66d6dcc33cc1cb3cc9920f
Young-Ho Kim <yghokim@umd.edu>
2019-10-10 23:18:30 -0400

    revised dependencies of services

--- src/measure/source/fitbit/FitbitHeartRateMeasure.ts
+++ src/measure/source/fitbit/FitbitHeartRateMeasure.ts
@@ -1,12 +1,16 @@
-import { DataSourceMeasure } from "../DataSource";
-import { measureService, MeasureSpecKey } from '../../../system/MeasureService';
-import { MeasureSpec } from "../../MeasureSpec";
-import { SourceDependency } from "../SourceDependency";
-import { FitbitSource } from "./FitbitSource";
+import {DataSourceMeasure} from '../DataSource';
+import {measureService, MeasureSpecKey} from '../../../system/MeasureService';
+import {MeasureSpec} from '../../MeasureSpec';
+import {FitbitSource} from './FitbitSource';
 
-export class FitbitHeartRateMeasure extends DataSourceMeasure{
-    get dependencies(): SourceDependency[] {
-        return [this.castedSource<FitbitSource>().makeCredentialDependency("heartrate")]
-    }
-    spec: MeasureSpec = measureService.getSpec(MeasureSpecKey.heart)
+export class FitbitHeartRateMeasure extends DataSourceMeasure {
+  spec: MeasureSpec = measureService.getSpec(MeasureSpecKey.heart);
+
+  async activateInSystem(): Promise<boolean> {
+    return this.castedSource<FitbitSource>().authenticate('heartrate');
+  }
+
+  async deactivatedInSystem(): Promise<boolean> {
+    return this.castedSource<FitbitSource>().revokeScope("heartrate")
+  }
 }
