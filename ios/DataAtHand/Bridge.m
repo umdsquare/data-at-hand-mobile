8a12959427eb2a7f02be5983cdb79d69eb56061d
Young-Ho Kim <yghokim@umd.edu>
2019-10-11 22:15:26 -0400

    implemented basic permision logic for HealthKit.

---
+++ ios/DataAtHand/Bridge.m
@@ -0,0 +1,13 @@
+//
+//  HealthKitManager.m
+//  DataAtHand
+//
+//  Created by Young-Ho Kim on 10/11/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
+
+@interface RCT_EXTERN_MODULE(HealthKitManager, NSObject)
+@end
---
+++ ios/DataAtHand/HealthKitManager.swift
@@ -0,0 +1,9 @@
+//
+//  HealthKitManager.swift
+//  DataAtHand
+//
+//  Created by Young-Ho Kim on 10/11/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+import Foundation
