7e96cf6870a8e46da2b52c7d3e1841303f3932b2
Young-Ho Kim <yghokim@umd.edu>
2020-03-17 20:20:54 -0400

    Improved screen capture timing more robust. Fixed ratio issue on Android.

--- src/state/logger.ts
+++ src/state/logger.ts
@@ -5,16 +5,19 @@ import { SystemLogger } from "@core/logging/SystemLogger"
 import { captureScreen } from "react-native-view-shot";
 import { moveFile, exists, mkdir } from "react-native-fs";
 import path from 'react-native-path'
-import { Dimensions, PixelRatio } from "react-native";
+import { Dimensions, PixelRatio, Platform } from "react-native";
 import { SettingsActionTypes, SetRecordLogsAction } from "@state/settings/actions";
+import { ExplorationDataActionType } from "./exploration/data/actions";
 
 const whitelistForScreenshot = [ExplorationActionType.SetTouchElementInfo];
+
+let logIdForScreenshot: string | null = null;
+let screenshotTimeout: NodeJS.Timeout | null = null;
 
 export const makeLogger = () => {
     return (api: MiddlewareAPI) => (next: Dispatch) => (action: ActionTypeBase) => {
 
         const prevState: ReduxAppState = api.getState()
-
         if (prevState.settingsState.recordLogs === true && (action.type.startsWith(USER_INTERACTION_ACTION_PREFIX) || action.type === ExplorationActionType.Reset)) {
             //
             const returnedValue = next(action)
@@ -32,32 +35,11 @@ export const makeLogger = () => {
                     }, logId, timestamp).then()
             } else {
                 SystemLogger.instance
-                    .logInteractionStateTransition(prevState.settingsState.serviceKey, action, action.type === ExplorationActionType.SetTouchElementInfo? undefined : nextExplorationState.info, logId, timestamp).then()
+                    .logInteractionStateTransition(prevState.settingsState.serviceKey, action, action.type === ExplorationActionType.SetTouchElementInfo ? undefined : nextExplorationState.info, logId, timestamp).then()
             }
 
             if (prevState.settingsState.recordLogs === true && prevState.settingsState.recordScreens === true && whitelistForScreenshot.indexOf(action.type as any) === -1) {
-                console.log("take screenshot")
-                setTimeout(() => {
-                    const pixelRatio = PixelRatio.get();
-                    const imageWidth = 300;
-                    const imageHeight = imageWidth * Dimensions.get('screen').height / Dimensions.get('screen').width
-                    captureScreen({
-                        format: 'jpg',
-                        quality: 0.5,
-                        width: Math.round(imageWidth / pixelRatio),
-                        height: Math.round(imageHeight / pixelRatio)
-                    }).then(async uri => {
-                        const directoryPath = path.resolve(SystemLogger.instance.logDirectoryPath, "screens")
-                        const directoryExists = await exists(directoryPath)
-                        if (directoryExists === false) {
-                            await mkdir(directoryPath)
-                        }
-
-                        return moveFile(uri, path.resolve(directoryPath, logId + ".jpg"))
-                    }, error => {
-                        console.log("Screenshot error: ", error)
-                    }).then()
-                }, 1000)
+                logIdForScreenshot = logId
             }
 
             return returnedValue
@@ -67,34 +49,70 @@ export const makeLogger = () => {
                 const timestamp = Date.now();
                 const logId = SystemLogger.instance.makeLogId(timestamp);
 
-                if(a.recordLogs === true){
+                if (a.recordLogs === true) {
                     //turn on
                     //turn on then log
                     const returnedValue = next(action)
 
                     SystemLogger.instance
-                    .logVerboseToInteractionStateTransition("LoggingTurnedOn", {
-                        info: prevState.explorationState.info,
-                        service: prevState.settingsState.serviceKey
-                    }, logId, timestamp).then()
+                        .logVerboseToInteractionStateTransition("LoggingTurnedOn", {
+                            info: prevState.explorationState.info,
+                            service: prevState.settingsState.serviceKey
+                        }, logId, timestamp).then()
 
                     return returnedValue
-                }else {
+                } else {
                     //turn off
                     SystemLogger.instance
-                    .logVerboseToInteractionStateTransition("LoggingTurnedOff", {
-                        info: prevState.explorationState.info,
-                        service: prevState.settingsState.serviceKey
-                    }, logId, timestamp).then()
+                        .logVerboseToInteractionStateTransition("LoggingTurnedOff", {
+                            info: prevState.explorationState.info,
+                            service: prevState.settingsState.serviceKey
+                        }, logId, timestamp).then()
 
                     return next(action)
                 }
 
-                
+
             }
 
             return next(action)
 
+        } else if (action.type === ExplorationDataActionType.FinishLoadingDataAction && logIdForScreenshot != null) {
+            if(screenshotTimeout){
+                console.log("skip the previous screenshot task.")
+                clearTimeout(screenshotTimeout)
+            }
+
+            const logId = logIdForScreenshot
+            logIdForScreenshot = null
+
+            screenshotTimeout = setTimeout(() => {
+                const pixelRatio = PixelRatio.get();
+                const imageWidth = 300;
+
+                const screenDimension = Dimensions.get('window')
+                const imageHeight = imageWidth * screenDimension.height / screenDimension.width
+                captureScreen({
+                    format: 'jpg',
+                    quality: Platform.OS == 'ios'? 0.5 : 1,
+                    width: Math.round(imageWidth / pixelRatio),
+                    height: Math.round(imageHeight / pixelRatio)
+                }).then(async uri => {
+                    const directoryPath = path.resolve(SystemLogger.instance.logDirectoryPath, "screens")
+                    const directoryExists = await exists(directoryPath)
+                    if (directoryExists === false) {
+                        await mkdir(directoryPath)
+                    }
+
+                    return moveFile(uri, path.resolve(directoryPath, logId + ".jpg"))
+                }, error => {
+                    console.log("Screenshot error: ", error)
+                }).then()
+                logIdForScreenshot = null
+                screenshotTimeout = null
+            }, 600)
+
+            return next(action);
         } else return next(action)
     }
 }
