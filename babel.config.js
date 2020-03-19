8de40c270e177484a9fb20ffb4496d7e0cad9ea5
Young-Ho Kim <yghokim@umd.edu>
2020-03-19 17:26:16 -0400

    Revised babel config to remove console logs only in production mode.

--- babel.config.js
+++ babel.config.js
@@ -1,22 +1,32 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      "module-resolver",
-      {
-        root: ['./'],
-        alias: {
-          '^@credentials/(.+)': "./credentials/\\1",
-          '^@style/(.+)': "./src/style/\\1",
-          '^@core/(.+)': "./src/core/\\1",
-          '^@state/(.+)': "./src/state/\\1",
-          '^@measure/(.+)': "./src/measure/\\1",
-          '^@components/(.+)': "./src/components/\\1",
-          '^@utils/(.+)': "./src/utils/\\1",
-          '^@assets/(.+)': "./assets/\\1",
+module.exports = function (api) {
+  const commonConfig = {
+    presets: [ 'module:metro-react-native-babel-preset' ],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: [ './' ],
+          alias: {
+            '^@credentials/(.+)': "./credentials/\\1",
+            '^@style/(.+)': "./src/style/\\1",
+            '^@core/(.+)': "./src/core/\\1",
+            '^@state/(.+)': "./src/state/\\1",
+            '^@measure/(.+)': "./src/measure/\\1",
+            '^@components/(.+)': "./src/components/\\1",
+            '^@utils/(.+)': "./src/utils/\\1",
+            '^@assets/(.+)': "./assets/\\1",
+          }
         }
-      }
-    ],
-    "transform-remove-console"
-  ]
-};
+      ]
+    ]
+  }
+
+  api.cache(true);
+  if (process.env.NODE_ENV === 'production' || process.env.BABEL_ENV === 'production') {
+    commonConfig.plugins = commonConfig.plugins.concat([
+      "transform-remove-console"
+    ])
+  }
+
+  return commonConfig
+}
