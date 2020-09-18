b4b02d7f70b3815da592c331ec39be4f2d5c0929
Young-Ho Kim <yghokim@umd.edu>
2020-09-18 16:39:52 -0400

    remove study configs and the backend URL

--- src/core/logging/common.ts
+++ src/core/logging/common.ts
@@ -1,17 +1,4 @@
 
-
-function readStudyConfig() {
-    try {
-        return require('@credentials/study.json')
-    } catch (ex) {
-        return null
-    }
-}
-
-const studyCredentials = readStudyConfig()
-
 export function getUploadServerHostUrl(): string | undefined {
-    if (studyCredentials != null && studyCredentials.backend_logging_url != null && studyCredentials.backend_logging_url.length > 0) {
-        return studyCredentials.backend_logging_url
-    } else return undefined
+    return undefined
 }
